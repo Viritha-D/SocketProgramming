1bcae07f36e42b68982d235948422892e776e703
Viritha Daggubati <145008999+Viritha-D@users.noreply.github.com>
2025-07-27 18:12:30 +0530

    Update server.py

--- Que1/server.py
+++ Que1/server.py
@@ -1,1 +1,23 @@
+from socket import *
+serverport=12000
+serversocket=socket(AF_INET,SOCK_STREAM)
+serversocket.bind(('',serverport))
+serversocket.listen(1)
+print(f"tcp server is ready to receieve on the port {serverport}")
+
+while True:
+    clientaddress,connectionsocket=serversocket.accept()
+    print(f"connection accepted from {clientaddress}")
+
+    message=connectionsocket.recv(2048)
+
+    print(f"message receieved is:{message.decode()}")
+
+    modifiedmessage=message.decode().upper()
+
+    connectionsocket.send(modifiedmessage.encode())
+
+    print(f"sent: {modifiedmessage}")
+
+    connectionsocket.close()
 
